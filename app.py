d66cc85a0f93e219b156803dc3c4a0105691d900
Rithik <96978578+rithik1808@users.noreply.github.com>
2025-04-21 18:17:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ def decrypt():
         return jsonify({"decrypted_data": decrypted})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
