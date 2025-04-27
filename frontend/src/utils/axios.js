e87ea74f5a76591317cebf8d2f0c2b03e7a0b0e7
rithik1808 <rithikrohith66@gmail.com>
2025-04-27 20:29:51 +0530

    URL from .env

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BaseUrl = "https://biometric-nine.vercel.app";
+const BaseUrl = import.meta.env.VITE_BASE_URL;
 
 const axiosInstance = axios.create({
   baseURL: `${BaseUrl}/api`,
